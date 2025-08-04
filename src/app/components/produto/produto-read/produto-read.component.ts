3e22b6e0d52ab4766ca679a808264334d4c14fa5
leokenta <leonardo.kenta02@gmail.com>
2025-08-04 15:43:49 -0300

    atualizado

--- src/app/components/produto/produto-read/produto-read.component.ts
+++ src/app/components/produto/produto-read/produto-read.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Produto } from '../produto.model';
 import { ProdutoService } from '../produto.service';
 
-
 @Component({
   selector: 'app-produto-read',
   templateUrl: './produto-read.component.html',
@@ -13,7 +12,16 @@ export class ProdutoReadComponent implements OnInit {
   filtro: string = '';
   produtos: Produto[] = [];
   produtosFiltrados: Produto[] = [];
-  displayedColumns = ['prodId', 'prodNome', 'prodPrecoCusto', 'prodPrecoVenda', 'prodQtdEstoque', 'prodCategoria', 'prodMarca', 'action'];
+  displayedColumns = [
+    'prodId',
+    'prodNome',
+    'prodPrecoCusto',
+    'prodPrecoVenda',
+    'prodQtdEstoque',
+    'prodCategoria',
+    'prodMarca',
+    'action'
+  ];
 
   constructor(private produtoService: ProdutoService) { }
 
@@ -21,20 +29,19 @@ export class ProdutoReadComponent implements OnInit {
     this.produtoService.read().subscribe(produtos => {
       this.produtos = produtos;
       this.produtosFiltrados = produtos;
-      
     });
   }
 
   aplicarFiltro(): void {
     const filtroLower = this.filtro.toLowerCase();
+
     this.produtosFiltrados = this.produtos.filter(produto =>
       produto.prodNome.toLowerCase().includes(filtroLower) ||
-      produto.prodPrecoCusto ||
-      produto.prodPrecoVenda ||
-      produto.prodQtdEstoque ||
+      produto.prodPrecoCusto.toString().includes(filtroLower) ||
+      produto.prodPrecoVenda.toString().includes(filtroLower) ||
+      produto.prodQtdEstoque.toString().includes(filtroLower) ||
       produto.prodCategoria.toLowerCase().includes(filtroLower) ||
       produto.prodMarca.toLowerCase().includes(filtroLower)
     );
   }
-
 }
